9842f14daa1d128f784aa3f8f27c78f8b71e2da0
Abhay Doke <abhaydoke09@gmail.com>
2017-09-05 11:04:41 -0400

    tensorboard refresh

--- data-flow-graphs/add.py
+++ data-flow-graphs/add.py
@@ -1,7 +1,7 @@
 import tensorflow as tf 
 
-int_add = tf.add(3,4)
-float_add = tf.add(5,6)
+int_add = tf.add(3,4, name="Int_Add")
+float_add = tf.add(5,6, name="Float_Add")
 
 sess = tf.Session()
 
@@ -16,6 +16,9 @@ to avoid using sess.close() we can use the with statement
 '''
 
 with tf.Session() as sess:
+	writer = tf.summary.FileWriter('./graphs',sess.graph)
 	print sess.run(float_add)
 	print sess.run(int_add)
+
+	writer.close()
 
