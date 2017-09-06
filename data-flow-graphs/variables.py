1b650302204b456762e34c7c8006a68851f9caa4
Abhay Doke <abhaydoke09@gmail.com>
2017-09-06 11:16:16 -0400

    assign to variavles

--- data-flow-graphs/variables.py
+++ data-flow-graphs/variables.py
@@ -13,3 +13,14 @@ with tf.Session() as sess:
 	print sess.run(b)
 	print sess.run(c)
 	print sess.run(d)[2:5]
+	print d.eval().shape
+
+
+w = tf.Variable(tf.zeros([10,10]))
+w_assign = w[2:8,:].assign(tf.ones([6,10]))
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+	sess.run(init)
+	sess.run(w)
+	sess.run(w_assign)
+	print w_assign.eval()
